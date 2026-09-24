cfb2ecdcb89c8270b0ce465fae678bcdead917e0
ROSANALRL <rosanaribeirogp@gmail.com>
2026-09-24 20:43:54 -0300

    style: adiciona estilos do componente formulario-desejo

--- lista-de-desejos/src/app/components/formulario-desejo/formulario-desejo.ts
+++ lista-de-desejos/src/app/components/formulario-desejo/formulario-desejo.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ListaDesejos } from '../../services/lista-desejos/lista-desejos';
 
 @Component({
-  imports: [],
   selector: 'app-formulario-desejo',
-  styleUrl: './formulario-desejo.css',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './formulario-desejo.html',
+  styleUrl: './formulario-desejo.css'
 })
-export class FormularioDesejo {}
+export class FormularioDesejoComponent {
+
+  formGroup = new FormGroup({
+    nome: new FormControl('', [Validators.required]),
+    prioridade: new FormControl('', [Validators.required])
+  });
+
+  constructor(private listaDesejosService: ListaDesejos) {}
+
+  salvarDesejo(): void {
+    if (this.formGroup.valid) {
+      console.log('Dados do formulário:', this.formGroup.value);
+    }
+  }
+}
