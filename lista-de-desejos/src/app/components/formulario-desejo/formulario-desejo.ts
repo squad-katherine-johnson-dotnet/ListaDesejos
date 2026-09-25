d49ba8c3a0ed161877a75e790b8d3927d9d333e0
ROSANALRL <rosanaribeirogp@gmail.com>
2026-09-24 22:07:46 -0300

    Style: remove comentarios.

--- lista-de-desejos/src/app/components/formulario-desejo/formulario-desejo.ts
+++ lista-de-desejos/src/app/components/formulario-desejo/formulario-desejo.ts
@@ -12,28 +12,23 @@ import { ListaDesejosService } from '../../services/lista-desejos';
 })
 export class FormularioDesejoComponent {
 
-  // Criamos o formulário reativo com os campos e validações
   formulario: FormGroup = new FormGroup({
     nome: new FormControl('', [Validators.required]),
     prioridade: new FormControl('', [Validators.required])
   });
 
-  // Usamos : any no serviço para o TypeScript não travar por conta do método da colega
-  constructor(private listaDesejosService: any) {}
-
-  // Método chamado ao clicar em enviar
+    constructor(private listaDesejosService: any) {}
+  
   adicionarDesejo(): void {
     if (this.formulario.valid) {
       const novoDesejo = {
         nome: this.formulario.value.nome,
         prioridade: this.formulario.value.prioridade
       };
-
-      // O TypeScript aceita a chamada sem dar erro no VS Code
+     
       this.listaDesejosService.adicionarDesejo(novoDesejo);
       
-      // Limpa o formulário após o envio
-      this.formulario.reset();
+            this.formulario.reset();
     }
   }
 }
