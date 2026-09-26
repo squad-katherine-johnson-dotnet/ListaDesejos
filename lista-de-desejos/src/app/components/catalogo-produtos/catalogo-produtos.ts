2ee2dbdf64d9f791ad1680bb6c7f9a93f975bc8c
mariana-mbs <mariana-mbs@outlook.com>
2026-09-26 19:19:54 -0300

    fix: corrige exibicao dos produtos no catalogo

--- lista-de-desejos/src/app/components/catalogo-produtos/catalogo-produtos.ts
+++ lista-de-desejos/src/app/components/catalogo-produtos/catalogo-produtos.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common'
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Produto } from '../../models/produto';
 import { RouterLink } from '@angular/router';
 import { ListaDesejosService } from '../../services/lista-desejos.service';
@@ -15,23 +15,31 @@ export class CatalogoProdutos implements OnInit {
 
   produtos: Produto[] = [];
 
-  constructor(private listaDesejosService: ListaDesejosService) { }
+  constructor(
+  private listaDesejosService: ListaDesejosService,
+  private cdr: ChangeDetectorRef
+) { }
 
   ngOnInit(): void {
     this.buscarProdutos();
   }
 
   buscarProdutos(): void {
+
     this.listaDesejosService.buscarProdutos().subscribe({
+
       next: (produtos) => {
         this.produtos = produtos;
-      },
+        this.cdr.detectChanges();
+},
       error: (erro) => {
+
         console.error('Erro ao buscar produtos:', erro);
       }
     });
   }
   adicionarDesejo(produto: Produto): void {
+    
     this.listaDesejosService.adicionarDesejo({
       produto,
       prioridade: 'media'
