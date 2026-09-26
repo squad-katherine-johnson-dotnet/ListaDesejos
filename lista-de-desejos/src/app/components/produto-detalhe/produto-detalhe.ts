4fd8fb5d7c13b5f2186879f55803aeedb50cc9aa
mariana-mbs <mariana-mbs@outlook.com>
2026-09-26 19:25:59 -0300

    feat: implementa detalhes do produto

--- lista-de-desejos/src/app/components/produto-detalhe/produto-detalhe.ts
+++ lista-de-desejos/src/app/components/produto-detalhe/produto-detalhe.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Produto } from '../../models/produto';
+import { ListaDesejosService } from '../../services/lista-desejos.service';
 
 @Component({
-  imports: [],
+  imports: [CommonModule, RouterLink],
   selector: 'app-produto-detalhe',
   styleUrl: './produto-detalhe.css',
   templateUrl: './produto-detalhe.html',
 })
-export class ProdutoDetalhe {}
+export class ProdutoDetalhe implements OnInit {
+
+  produto: Produto | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private listaDesejosService: ListaDesejosService,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.listaDesejosService.buscarProdutoPorId(id).subscribe({
+      next: (produto) => {
+        this.produto = produto;
+        this.cdr.detectChanges();
+      },
+      error: (erro) => {
+        console.error('Erro ao buscar produto:', erro);
+      }
+    });
+  }
+}
